a2d23825e37321be316d5495321169dd577f1075
sivasathiya <sathyasiva369@gmail.com>
2017-07-01 14:28:47 +0530

    Create h.cpp

---
+++ h.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a;
+cin>>a;
+if(a>0)
+{
+cout<<"positive";
+}
+else if(a<0)
+{
+cout<<"nagative";
+}
+else
+{
+cout<<"zero";
+}
+getch();
+return 0;
+}
